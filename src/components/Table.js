0cec2dd4a352238469c8962aeb4356ab028eec27
Manish Chandra <manishchandra58@gmail.com>
2021-06-20 21:53:07 +0530

    Table sorting added

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,71 @@
 import React, { useEffect, useState } from 'react';
-
+const SortingDirection = {
+    ASCENDING : "ASCENDING",
+    DESCENDING : "DESCENDING",
+    UNSORTED : "UNSORTED"
+  }
+  Object.freeze(SortingDirection);
+  const sortData = (data,sortKey,sortingDirection)=>{
+      data.sort((a,b)=>{
+          const valueA = a[sortKey];
+          const valueB = b[sortKey];
+          if (
+            sortingDirection === SortingDirection.UNSORTED ||
+            sortingDirection === SortingDirection.ASCENDING
+          ) {
+            if (valueA < valueB) return -1;
+            if (valueA > valueB) return 1;
+            return 0;
+          } else {
+            if (valueA > valueB) return -1;
+            if (valueA < valueB) return 1;
+            return 0;
+          }
+      })
+  }
+  const getNextSortingDirection = (sortingDirection)=>{
+    if (
+        sortingDirection === SortingDirection.UNSORTED ||
+        sortingDirection === SortingDirection.ASCENDING
+      ){
+          return SortingDirection.DESCENDING;
+      }
+      return SortingDirection.ASCENDING;
+  }
 function Table(props) {
     const {users} = props;
     const [flattenedData,setFlattenedData] = useState({
         headers:[],
         data: []
     });
+    const [sortingDirections, setSortingDirections] = useState({});
+    const sortColumn = (sortKey)=>{
+        const newFlattenedData = {
+            ...flattenedData,
+            data: [...flattenedData.data]
+        }
+        const currentSortingDirection = sortingDirections[sortKey];
+        sortData(newFlattenedData.data,sortKey,currentSortingDirection);
+        const nextSortingDirection = getNextSortingDirection(currentSortingDirection);
+        const newSortingDirection = {...sortingDirections};
+        newSortingDirection[sortKey] = nextSortingDirection;
+
+        setFlattenedData(newFlattenedData);
+        setSortingDirections(newSortingDirection);
+    }
+    
     useEffect(()=>{
         const ourFlattenedData = flattenedTableData();
         // console.log(ourFlattenedData);
         setFlattenedData(ourFlattenedData);
+        const {headers} = ourFlattenedData;
+        const ourSortingDirection = {};
+        for(const header of headers){
+            ourSortingDirection[header] = SortingDirection.UNSORTED;
+        }
+        setSortingDirections(ourSortingDirection);
 
-    },[users]);
+    },[]);
     const flattenedTableData = ()=>{
         const data = [];
         // console.log(users)
@@ -28,13 +82,14 @@ function Table(props) {
         objectKeys.push("id",'name','email','phone','website','company')
         return objectKeys.map((x)=>{return x.toUpperCase()});
     }
+  
     return (
         <>
         <table>
             <thead>
             <tr>
                 {flattenedData.headers.map((dataString,index)=>(
-                    <th key={`index-${dataString}`}>
+                    <th key={`${index}-${dataString}`} onClick={() => {sortColumn(dataString.toLowerCase())}}>
                         {dataString}
                     </th>
                      ))}
